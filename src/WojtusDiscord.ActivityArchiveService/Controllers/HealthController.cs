9f95ebd93445f0b996591f5d3202a69d4024100a
Wiktor Kowalski <kowalskiwiktor98@gmail.com>
2022-10-13 22:27:27 +0200

    feat: add ActivityArchiveService

---
+++ src/WojtusDiscord.ActivityArchiveService/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WojtusDiscord.ActivityArchiveService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHealth()
+        {
+            return Ok(new { status = "ActivityArchiveService is running" });
+        }
+    }
+}
