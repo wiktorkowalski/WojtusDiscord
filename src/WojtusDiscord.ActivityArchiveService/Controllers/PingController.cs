b1b214df892412582eb90221d1288631f192e272
Wiktor Kowalski <kowalskiwiktor98@gmail.com>
2022-10-22 22:35:45 +0200

    feat: ping controller

---
+++ src/WojtusDiscord.ActivityArchiveService/Controllers/PingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WojtusDiscord.ActivityArchiveService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Pong");
+        }
+    }
+}
